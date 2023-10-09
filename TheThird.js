88421faac0f450811408ae3ad07489eeb0237764
larapable <143684968+larapable@users.noreply.github.com>
2023-10-09 21:02:46 +0800

    Create TheThird.js

---
+++ TheThird.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import Display from './Display';
+
+class TheThird extends React.Component {
+  state = {
+    message: ""
+  };
+
+  onChange = event => {
+    this.setState({message: event.target.value});
+  };
+  
+  render() {
+    
+const {message} = this.state;
+
+return (
+<Grid>
+<div paddingBottom='10px' style={{ display: 'flex', flexDirection: 'column'}}>
+      <div paddingBottom='10px'>
+      <input
+          type='text'
+          value={message}
+          onChange={this.onChange}
+          style={{ width: '800px', height: '40px',border:'3px solid black',borderRadius:'6px' }}
+        />
+    </div>
+    </div>
+    <Display {...this.props} message={message} bgColor="green" child="third" />
+    </Grid>
+);
+  }
+}
+
+export default TheThird;
